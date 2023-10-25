f7ea97841b9a494651761b71b44d53497d18a541
Anukul Pandey <anukul.030601@gmail.com>
2023-10-25 14:45:44 +0530

    feat: removed unused gqls

--- src/index.ts
+++ src/index.ts
@@ -7,5 +7,4 @@ export * as rpc from './rpc';
 export * as hooks from './hooks';
 export * as utils from './utils';
 export * as store from './store';
-export * as graphql from './graphql';
 export * as Components from './components';
