36f4b38ad0798ea9e7faeab8f033317894217e7b
matjazonline <matjazonline@gmail.com>
2023-11-14 22:04:43 +0100

    fix useTransactionHistory.ts

--- src/hooks/useTransactionHistory.ts
+++ src/hooks/useTransactionHistory.ts
@@ -1,15 +1,23 @@
-import { reefState } from '@reef-chain/util-lib';
+import { reefState, StatusDataObject } from '@reef-chain/util-lib';
+import { useEffect, useState } from 'react';
 import { TokenTransfer } from '../state';
 import { useObservableState } from './useObservableState';
 
-type UseTxHistory = TokenTransfer[];
+const { FeedbackStatusCode } = reefState;
+
+type UseTxHistory = [TokenTransfer[], boolean];
 export const useTxHistory = (): UseTxHistory => {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const txHistory:any = useObservableState(reefState.selectedTransactionHistory_status$);
-  let txHistoryData;
-  if (txHistory && txHistory._status[0].code === 6) {
-    txHistoryData = txHistory.data;
-  }
+  const txHistoryStatus:StatusDataObject<TokenTransfer[]> = useObservableState(reefState.selectedTransactionHistory_status$);
+  const [history, setHistory] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  return txHistoryData;
+  useEffect(() => {
+    setLoading(!txHistoryStatus || txHistoryStatus?.hasStatus(FeedbackStatusCode.LOADING));
+    if (txHistoryStatus) {
+      setHistory(txHistoryStatus.data);
+    }
+  }, [txHistoryStatus]);
+
+  return [history, loading];
 };
