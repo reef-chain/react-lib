9f20f9b08ad0dd797343a0d7f82cff5ce14f8f02
Sajal Sharma <sajalsharma2408@gmail.com>
2023-12-04 21:30:26 +0530

    fix: lint errors

--- src/utils/transactionUtil.ts
+++ src/utils/transactionUtil.ts
@@ -1,10 +1,10 @@
 /* eslint-disable prefer-promise-reject-errors */
 
-import { Provider } from '@reef-chain/evm-provider';
-import { BigNumber } from 'ethers';
-import {  ReefSigner } from '../state';
-import { Network } from '../state/network';
-import {network as nw} from "@reef-chain/util-lib";
+import { Provider } from "@reef-chain/evm-provider";
+import { BigNumber } from "ethers";
+import { network as nw } from "@reef-chain/util-lib";
+import { ReefSigner } from "../state";
+import { Network } from "../state/network";
 
 export type TxStatusHandler = (status: TxStatusUpdate) => void;
 
@@ -31,26 +31,27 @@ export const handleErr = (
   txIdent: string,
   txHash: string,
   txHandler: TxStatusHandler,
-  signer: ReefSigner,
+  signer: ReefSigner
 ): void => {
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
   let message = e.message || e;
   let code = TX_STATUS_ERROR_CODE.ERROR_UNDEFINED;
   if (
-    message
-    && (message.indexOf('-32603: execution revert: 0x') > -1
-      || message?.indexOf('InsufficientBalance') > -1)
+    message &&
+    (message.indexOf("-32603: execution revert: 0x") > -1 ||
+      message?.indexOf("InsufficientBalance") > -1)
   ) {
-    message = 'You must allow minimum 60 REEF on account for Ethereum VM transaction even if transaction fees will be much lower.';
+    message =
+      "You must allow minimum 60 REEF on account for Ethereum VM transaction even if transaction fees will be much lower.";
     code = TX_STATUS_ERROR_CODE.ERROR_MIN_BALANCE_AFTER_TX;
   }
-  if (message && message?.startsWith('1010')) {
-    message = 'Balance too low.';
+  if (message && message?.startsWith("1010")) {
+    message = "Balance too low.";
     code = TX_STATUS_ERROR_CODE.ERROR_BALANCE_TOO_LOW;
   }
-  if (message && message?.startsWith('balances.InsufficientBalance')) {
-    message = 'Balance too low for transfer and fees.';
+  if (message && message?.startsWith("balances.InsufficientBalance")) {
+    message = "Balance too low for transfer and fees.";
     code = TX_STATUS_ERROR_CODE.ERROR_BALANCE_TOO_LOW;
   }
   if (code === TX_STATUS_ERROR_CODE.ERROR_UNDEFINED) {
@@ -64,8 +65,16 @@ export const handleErr = (
   });
 };
 
-export const nativeTransfer = async (amount: string, destinationAddress: string, provider: Provider, signer: ReefSigner): Promise<void> => {
-  const transfer = provider.api.tx.balances.transfer(destinationAddress, amount);
+export const nativeTransfer = async (
+  amount: string,
+  destinationAddress: string,
+  provider: Provider,
+  signer: ReefSigner
+): Promise<void> => {
+  const transfer = provider.api.tx.balances.transfer(
+    destinationAddress,
+    amount
+  );
   const send = new Promise<void>((resolve, reject) => {
     transfer.signAndSend(
       signer.address,
@@ -77,7 +86,7 @@ export const nativeTransfer = async (amount: string, destinationAddress: string,
         if (status.status.isInBlock) {
           resolve();
         }
-      },
+      }
     );
   });
   await send;
@@ -88,7 +97,7 @@ export const sendToNativeAddress = (
   signer: ReefSigner,
   toAmt: BigNumber,
   to: string,
-  txHandler: TxStatusHandler,
+  txHandler: TxStatusHandler
 ): string => {
   const txIdent = Math.random().toString(10);
   const transfer = provider.api.tx.balances.transfer(to, toAmt.toString());
@@ -106,24 +115,34 @@ export const sendToNativeAddress = (
             isComplete: res.isFinalized,
             addresses: [signer.address, to],
           });
-        },
+        }
       )
       .catch((e) => {
-        console.log('sendToNativeAddress err=', e);
-        handleErr(e, txIdent, '', txHandler, signer);
+        console.log("sendToNativeAddress err=", e);
+        handleErr(e, txIdent, "", txHandler, signer);
       });
   });
 
   return txIdent;
 };
 
-export const getExtrinsicUrl = (extrinsic: {id:string}, network: Network = nw.AVAILABLE_NETWORKS.mainnet): string => {
-  const [blockHeight, extrinsicIndex] = extrinsic.id.split('-');
+export const getExtrinsicUrl = (
+  extrinsic: { id: string },
+  network: Network = nw.AVAILABLE_NETWORKS.mainnet
+): string => {
+  const [blockHeight, extrinsicIndex] = extrinsic.id.split("-");
   return `${network.reefscanUrl}/extrinsic/${blockHeight}/${extrinsicIndex}`;
 };
-export const getTransferUrl = (extrinsic: {id:string}, event:{index:string}, network: Network =  nw.AVAILABLE_NETWORKS.mainnet): string => {
-  const [blockHeight, extrinsicIndex] = extrinsic.id.split('-');
+export const getTransferUrl = (
+  extrinsic: { id: string },
+  event: { index: string },
+  network: Network = nw.AVAILABLE_NETWORKS.mainnet
+): string => {
+  const [blockHeight, extrinsicIndex] = extrinsic.id.split("-");
   return `${network.reefscanUrl}/transfer/${blockHeight}/${extrinsicIndex}/${event.index}`;
 };
 
-export const getContractUrl = (address: string, network: Network =  nw.AVAILABLE_NETWORKS.mainnet): string => `${network.reefscanUrl}/contract/${address}`;
+export const getContractUrl = (
+  address: string,
+  network: Network = nw.AVAILABLE_NETWORKS.mainnet
+): string => `${network.reefscanUrl}/contract/${address}`;
