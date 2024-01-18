ba4b641c6a4e8b101947566a1f9a268f14bb9cc6
Anukul Pandey <anukul.030601@gmail.com>
2024-01-18 17:40:36 +0530

    Revert "feat: component center schema"
    This reverts commit ebbd4b3b767e950eee8660f563042824eff1adee.

--- src/components/common/Display/index.tsx
+++ src/components/common/Display/index.tsx
@@ -105,7 +105,7 @@ export const Border: React.FC<Size> = ({
   <div className={`border border-rad p-${size}`}>{children}</div>
 );
 
-export const ComponentCenter: React.FC<React.PropsWithChildren<{children:JSX.Element}>> = ({
+export const ComponentCenter: React.FC<React.PropsWithChildren<{}>> = ({
   children,
 }): JSX.Element => (
   <CenterColumn>
