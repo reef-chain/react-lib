12acdaf503a02bf4ed13ea4e28a53e5bb98d3ea0
Anukul Pandey <62092256+anukulpandey@users.noreply.github.com>
2024-03-05 16:12:59 +0530

    feat: input for usd (#37)
    * feat: input for usd
    
    * chore: cleanup
    
    * feat: only display if token is reef
    
    * feat: show value of 1 reef
    
    * feat: fixed to 2 decimal
    
    ---------
    
    Co-authored-by: matjazonline <matjazonline@gmail.com>

--- src/components/Transfer/Send.tsx
+++ src/components/Transfer/Send.tsx
@@ -33,6 +33,8 @@ import "../PoolActions/pool-actions.css";
 import TokenField from "../PoolActions/TokenField";
 import "./Send.css";
 import SendPopup from "../PoolActions/ConfirmPopups/Send";
+import { retrieveReefCoingeckoPrice } from "../../api";
+import UsdAmountField from "../PoolActions/UsdAmountField";
 
 interface Send {
   tokens: Token[];
@@ -191,6 +193,8 @@ export const Send = ({
   const [status, setStatus] = useState("Send");
   const [isLoading, setLoading] = useState(false);
   const [isAmountPristine, setAmountPristine] = useState(true);
+  const [amountInUsd,setAmountInUsd] = useState("");
+  const [reefPrice,setReefPrice]= useState(0);
 
   const getInitToken = (): TokenWithAmount => {
     if (tokenAddress) {
@@ -209,6 +213,14 @@ export const Send = ({
 
     return reefTokenWithAmount();
   };
+
+  useEffect(()=>{
+    const fetchReefPrice = async()=>{
+      const _reefPrice = await retrieveReefCoingeckoPrice();
+      setReefPrice(_reefPrice);
+    }
+    fetchReefPrice();
+  },[])
 
   const [token, setToken] = useState(getInitToken());
 
@@ -229,9 +241,13 @@ export const Send = ({
     signer.balance
   );
 
-  const onAmountChange = (amount: string, token: TokenWithAmount): void => {
+  const onAmountChange = (amount: string, token: TokenWithAmount,isUSDChanged?:boolean): void => {
     setToken({ ...token, amount });
     setAmountPristine(false);
+    if(!isUSDChanged){
+      let calculatedUsdFromReef = reefPrice*parseFloat(amount);
+      setAmountInUsd(calculatedUsdFromReef>1?calculatedUsdFromReef.toFixed(2):calculatedUsdFromReef.toFixed(4));
+    }
   };
 
   const onSend = async (): Promise<void> => {
@@ -308,6 +324,13 @@ export const Send = ({
     onAmountChange(String(amount), token);
   };
 
+  const handleUsdAmount = (e)=>{
+    const inputAmount = e;
+    const calculatedReef = parseFloat(inputAmount)/reefPrice;
+    onAmountChange(String(calculatedReef), token,true);
+    setAmountInUsd(inputAmount);
+  }
+
   const [isPopupOpen, setPopupOpen] = useState(false);
 
   useEffect(()=>{  
@@ -372,6 +395,11 @@ export const Send = ({
       {!isAmountPristine && !existentialValidity.valid && (
         <div className="send__error">{existentialValidity.message}</div>
       )}
+
+    {token.address==REEF_ADDRESS &&  <div className="uik-pool-actions__tokens">
+          <UsdAmountField onInput={handleUsdAmount}
+          value={amountInUsd.toString()} reefPrice = {reefPrice.toString()} />
+    </div>}
 
       <div className="uik-pool-actions__slider">
         <Uik.Slider
