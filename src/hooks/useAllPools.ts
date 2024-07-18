b12d068fcc611a7d3ddce9c152d872cec4113623
Anukul Pandey <62092256+anukulpandey@users.noreply.github.com>
2024-07-18 18:38:01 +0530

    feat: icons resolver (#58)
    * feat: icons resolver
    
    * chore: cleanup
    
    * bump util-lib
    
    ---------
    
    Co-authored-by: matjazonline <matjazonline@gmail.com>

--- src/hooks/useAllPools.ts
+++ src/hooks/useAllPools.ts
@@ -3,8 +3,9 @@ import { useState } from 'react';
 import { ALL_POOLS, PoolQueryObject } from '../graphql/pools';
 import { PoolWithReserves } from '../state';
 import { graphqlRequest } from '../graphql/utils';
-import { getIconUrl } from '../components/common/Icons';
 import { useAsyncEffect } from './useAsyncEffect';
+import { tokenIconUtils } from '@reef-chain/util-lib';
+import { getIconUrl } from '../components/common/Icons';
 
 export const getAllPoolsQuery = (): PoolQueryObject => ({
   query: ALL_POOLS,
@@ -12,19 +13,38 @@ export const getAllPoolsQuery = (): PoolQueryObject => ({
 });
 export const useAllPools = (httpClient: AxiosInstance): PoolWithReserves[] => {
   const [allPools, setAllPools] = useState([]);
-  const [poolsCount,setPoolsCount] = useState(0);
+  const [poolsCount, setPoolsCount] = useState(0);
+
   const getAllPoolsQry = getAllPoolsQuery();
 
-  useAsyncEffect(async()=>{
-       const response = await graphqlRequest(httpClient, getAllPoolsQry);
+  useAsyncEffect(async () => {
+    let emptyTokenIconsAddresses: string[] = []
+    let tokenIconsMap={};
+
+    const response = await graphqlRequest(httpClient, getAllPoolsQry);
+    response.data.data?.allPools.map((pool) => {
+      if (pool.iconUrl1 == "") emptyTokenIconsAddresses.push(pool.token1);
+      else tokenIconsMap[pool.token1]=pool.iconUrl1
+      if (pool.iconUrl2 == "") emptyTokenIconsAddresses.push(pool.token2);
+      else tokenIconsMap[pool.token2]=pool.iconUrl2
+    })
+
+    if(emptyTokenIconsAddresses.length){
+      const _tokenIconsMap = await tokenIconUtils.resolveTokenUrl(emptyTokenIconsAddresses);
+      tokenIconsMap={
+        ..._tokenIconsMap,
+        ...tokenIconsMap
+      }
+    }
+
     const pools = response.data.data?.allPools.map((pool) => ({
       ...pool,
-      iconUrl1: pool.iconUrl1 === '' ? getIconUrl(pool.token1) : pool.iconUrl1,
-      iconUrl2: pool.iconUrl2 === '' ? getIconUrl(pool.token2) : pool.iconUrl2,
+      iconUrl1: pool.iconUrl1 === '' ? tokenIconsMap[pool.token1]??getIconUrl(pool.token1)  : pool.iconUrl1,
+      iconUrl2: pool.iconUrl2 === '' ? tokenIconsMap[pool.token2]??getIconUrl(pool.token2)  : pool.iconUrl2,
     }));
-    if(allPools.length!==pools.length)setPoolsCount(pools.length);
+    if (allPools.length !== pools.length) setPoolsCount(pools.length);
     setAllPools(pools);
-  },[poolsCount])
+  }, [poolsCount])
 
   return allPools;
 };
